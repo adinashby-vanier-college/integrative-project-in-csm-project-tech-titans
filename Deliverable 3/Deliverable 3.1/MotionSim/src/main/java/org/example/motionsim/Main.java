9cdcdea63cd43d06a14b16c1517cfc906e67b0a5
AlKhMi1 <michael.khouri06@gmail.com>
2025-03-15 14:33:03 -0400

    buttons and labels
    added the buttons and labels to the main menu.

--- Deliverable 3/Deliverable 3.1/MotionSim/src/main/java/org/example/motionsim/Main.java
+++ Deliverable 3/Deliverable 3.1/MotionSim/src/main/java/org/example/motionsim/Main.java
@@ -6,11 +6,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.lang.classfile.Label;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/motionsim/IPCSMFXMLGame.fxml"));
+        Button PlayB = new Button();
+        Button SettB = new Button();
+        Button LoadB = new Button();
+        Button ExitB = new Button();
+        CheckBox Mute = new CheckBox();
+        Label TechL = new Label();
+        Label Title = new Label();
+        Label Score = new  Label();
+        VBox vbox = new VBox(Title,PlayB,SettB,LoadB,ExitB);
         Scene scene = new Scene(fxmlLoader.load());
         stage.setResizable(false);
 //        stage.setFullScreen(true);
