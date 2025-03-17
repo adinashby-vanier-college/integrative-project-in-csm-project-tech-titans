73794948445fcd753673ec50662986f4c158f4cd
Alessandro Pomponi <alescode1@gmail.com>
2025-03-17 10:15:57 -0400

    fixed module error

--- Deliverable 3/Deliverable 3.1/MotionSim/src/main/java/module-info.java
+++ Deliverable 3/Deliverable 3.1/MotionSim/src/main/java/module-info.java
@@ -4,10 +4,7 @@ module org.example.motionsim {
     requires javafx.fxml;
 
     opens org.example.motionsim.Controller to javafx.fxml;
-   // opens org.example.motionsim.Logic to javafx.graphics
-    opens org.example.motionsim.Logic to javafx.fxml;
     exports org.example.motionsim;
-    exports org.example.motionsim.Logic;
     exports org.example.motionsim.Controller;
     exports org.example.motionsim.Model;
     opens org.example.motionsim.Model to javafx.fxml;
