3c7ede1e9a020323c55c196dda976c126bc5df9e
Alessandro Pomponi <alescode1@gmail.com>
2025-05-04 21:25:37 -0400

    Added full visibility and distinction between Dev 3.1, 3.2, 3.3.

---
+++ Deliverable 3/Deliverable 3.1/MotionSim/src/main/java/org/example/motionsim/Main.java
@@ -0,0 +1,27 @@
+package org.example.motionsim;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/motionsim/SettingsScreen.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 1430,800);
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
